da014b1783aa535a4b7bf27cdcfbe4a723c3d743
Lili Feng <lili.feng@colorado.edu>
2017-12-07 11:41:07 -0700

    Dec 7th, 2017

--- monthlst.py
+++ monthlst.py
@@ -1,13 +1,11 @@
 import obspy
 
-start_date  ='19910101'
-end_date    ='20101201'
+start_date  ='20110101'
+end_date    ='20170601'
 
-outfname    = 'ASN/Alaska_month_1991_2010.lst'
-
+outfname    = 'ASN/Alaska_month_2011_2017_6.lst'
 stime       = obspy.UTCDateTime(start_date)
 etime       = obspy.UTCDateTime(end_date)
-
 
 with open(outfname, 'w') as fid:
     while(stime <= etime):
