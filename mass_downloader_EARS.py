f8bae4ff7a0137b48d3dad2b28df2e70ad63352d
Lili Feng <lili.feng@colorado.edu>
2020-07-09 13:57:57 -0600

    tests for mass downloader

--- mass_downloader_EARS.py
+++ mass_downloader_EARS.py
@@ -3,19 +3,19 @@ from obspy.clients.fdsn.mass_downloader import RectangularDomain, \
     Restrictions, MassDownloader
 
 # Rectangular domain containing parts of southern Germany.
-domain = RectangularDomain(minlatitude=52., maxlatitude=72.5, minlongitude=-172., maxlongitude=-122.)
+domain = RectangularDomain(minlatitude=-17.78, maxlatitude=4.15, minlongitude=23.00, maxlongitude=40.2)
 
 restrictions = Restrictions(
     # Get data for a whole year.
-    starttime=obspy.UTCDateTime(2017, 6, 11),
-    endtime=obspy.UTCDateTime(2017, 6, 21),
+    starttime=obspy.UTCDateTime(1994, 8, 5),
+    endtime=obspy.UTCDateTime(1994, 8, 10),
     # Chunk it to have one file per day.
     chunklength_in_sec=86400,
     # Considering the enormous amount of data associated with continuous
     # requests, you might want to limit the data based on SEED identifiers.
     # If the location code is specified, the location priority list is not
     # used; the same is true for the channel argument and priority list.
-    network="TA", station="H20K", location="", channel="LH?",
+    channel="LH?",
     # The typical use case for such a data set are noise correlations where
     # gaps are dealt with at a later stage.
     reject_channels_with_gaps=False,
